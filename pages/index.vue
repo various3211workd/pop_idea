60b68ab8d13f18dcec95989c3bf616d88fefc649
various3211workd <kd1229568@st.kobedenshi.ac.jp>
2019-02-10 15:52:11 +0900

    [Add] loading screen

--- pages/index.vue
+++ pages/index.vue
@@ -1,100 +1,125 @@
 <template>
-  <div style="background-color: #333;">
-    <nav class="navbar" role="navigation" aria-label="main navigation">
-      <div class="navbar-brand">
-        <a class="navbar-item" href="https://elastic-gates-511a51.netlify.com" target="_blank">
-          <p style="font-size: 30px; font-family: Impact, Charcoal;">PoP IdeA</p>
-        </a>
-        <a href="https://github.com/various3211workd/pop_idea" target="_blank">
-          <font-awesome-icon class="icon is-large has-text-dark" :icon="[ 'fab', 'github' ]" />
-        </a>
-      </div>
-    </nav>
+  <div>
+    <div v-show="loading" style="background-color: #330066; width: 100vw; height: 100vh;">
+      <br><br><br><br><br><br><br><br>
+      <self-building-square-spinner
+        :animation-duration="2000"
+        :size="40"
+        :color="'#ff1d5e'"
+        style="margin: 0 auto"
+      />
+    </div>
+    <div v-show="!loading" style="background-color: #333;">
+      <nav class="navbar" role="navigation" aria-label="main navigation">
+        <div class="navbar-brand">
+          <a class="navbar-item" href="https://elastic-gates-511a51.netlify.com" target="_blank">
+            <p style="font-size: 30px; font-family: Impact, Charcoal;">PoP IdeA</p>
+          </a>
+          <a href="https://github.com/various3211workd/pop_idea" target="_blank">
+            <font-awesome-icon class="icon is-large has-text-dark" :icon="[ 'fab', 'github' ]" />
+          </a>
+        </div>
+      </nav>
 
-    <div class="container">
-      <div id="app">
-        <div class="content has-text-centered">
-          
-          <!-- view compoment -->
-          <div v-if="nextTodoId > this.num + 1">
-            <!-- commit streat -->       
-            <div class="streat">
-              <img :src="commit_icon" />
-            </div><br><br>
-            <font-awesome-icon class="icon is-large shoe" icon="shoe-prints" />
-            
-            <!-- modle window -->  
-            <label for="trigger" class="open_button">Result</label>
-            <div class="modal_wrap">
-            <input id="trigger" type="checkbox" :checked="this.pop_flag">
-              <div class="modal_overlay">
-                <label for="trigger" class="modal_trigger"></label>
-                <div class="modal_content">
-                  <label for="trigger" class="close_button">✖️</label>
-                  <h2>Congrats!!</h2>
-                  <h3>START</h3><br>
-                  <div v-for="item in list" :key="item.id - 1">
-                    <p>{{ item.text }}</p>
-                    <font-awesome-icon class="icon has-text-info" icon="arrow-down" />
+      <div class="container">
+        <div id="app">
+          <div class="content has-text-centered">
+
+            <!-- page first open view -->
+            <div v-if="first_flag == true">
+              <div class="modal_wrap">
+              <input id="trigger" type="checkbox" :checked="true">
+                <div class="modal_overlay">
+                  <label for="trigger" class="modal_trigger"></label>
+                  <div class="modal_content">
+                    <label for="trigger" class="close_button"  v-on:click="desableFirst()">✖️</label>
+                    <h2>使い方説明</h2>
                   </div>
-                  <h3>END</h3>
-
-                  <h3>おすすめの組み合わせ
-                    <br>
-                    {{ list[Math.floor(Math.random() * list.length)].text }}
-                     + {{ list[Math.floor(Math.random() * list.length)].text }}
-                  </h3>
                 </div>
               </div>
             </div>
-          </div>
-          
-          <br>
 
-          <!-- error message view -->
-          <div v-if="this.error_message" class="error_message has-text-warning">
-            <div class="notification is-primary" style="width: 30%; margin: auto;">
-              <button class="delete"></button>
-              <strong>{{ this.error_message }}</strong>
-            </div>
-          </div>
-          
-          <br><br><br><br><br><br><br>
-
-          <!-- draggble input form -->
-          <draggable v-model="list" element="ul" :options="{animation:300}">
-            <div v-for="item in list" :key="item.id">
-              <input v-model="item.text" class="input is-large" type="text" style="width: 30%">
-              <br><br><br><br>
+            <!-- view compoment -->
+            <div v-if="nextTodoId > this.num + 1">
+              <!-- commit streat -->       
+              <div class="streat">
+                <img :src="commit_icon" />
+              </div><br><br>
               <font-awesome-icon class="icon is-large shoe" icon="shoe-prints" />
-              <br><br><br>
-            </div>
-            <br><br>
-          </draggable>
+              
+              <!-- modal window -->    
+              <label for="trigger" class="open_button">Result</label>
+              <div class="modal_wrap">
+              <input id="trigger" type="checkbox" :checked="this.pop_flag">
+                <div class="modal_overlay">
+                  <label for="trigger" class="modal_trigger"></label>
+                  <div class="modal_content">
+                    <label for="trigger" class="close_button">✖️</label>
+                    <h2>Congrats!!</h2>
+                    <h3>START</h3><br>
+                    <div v-for="item in list" :key="item.id - 1">
+                      <p>{{ item.text }}</p>
+                      <font-awesome-icon class="icon has-text-info" icon="arrow-down" />
+                    </div>
+                    <h3>END</h3>
 
-          <!-- view signal graph, walk and stop button -->
-          <div v-if="nextTodoId <= this.num + 1">
-            <div class="sticky">
-              <img :src="this.signal_icon" />
-              <div class="cover">
-                <br><br>
-                <button class="button is-success is-large" v-on:click="doAdd()">
-                  <font-awesome-icon class="icon is-large" :icon="[ 'fas', 'walking' ]" />
-                </button>
-                <br><br>
-                <button class="button is-danger is-large" v-on:click="doRemove(item)">
-                  <font-awesome-icon class="icon is-large" :icon="[ 'fas', 'male' ]" />
-                </button>
-                <br><br>
+                    <h3>おすすめの組み合わせ
+                      <br>
+                      {{ list[Math.floor(Math.random() * list.length)].text }}
+                      + {{ list[Math.floor(Math.random() * list.length)].text }}
+                    </h3>
+                  </div>
+                </div>
               </div>
             </div>
-          </div>
+            
+            <br>
 
-          <br><br><br><br><br>
-          
-          <!-- view streat graph -->
-          <div class="streat">
-            <img :src="streat_icon" />
+            <!-- error message view -->
+            <div v-if="this.error_message" class="error_message has-text-warning">
+              <div class="notification is-primary" style="width: 30%; margin: auto;">
+                <button class="delete"></button>
+                <strong>{{ this.error_message }}</strong>
+              </div>
+            </div>
+            
+            <br><br><br><br><br><br><br>
+
+            <!-- draggble input form -->
+            <draggable v-model="list" element="ul" :options="{animation:300}">
+              <div v-for="item in list" :key="item.id">
+                <input v-model="item.text" class="input is-large" type="text" style="width: 30%">
+                <br><br><br><br>
+                <font-awesome-icon class="icon is-large shoe" icon="shoe-prints" />
+                <br><br><br>
+              </div>
+              <br><br>
+            </draggable>
+
+            <!-- view signal graph, walk and stop button -->
+            <div v-if="nextTodoId <= this.num + 1">
+              <div class="sticky">
+                <img :src="this.signal_icon" />
+                <div class="cover">
+                  <br><br>
+                  <button class="button is-success is-large" v-on:click="doAdd()">
+                    <font-awesome-icon class="icon is-large" :icon="[ 'fas', 'walking' ]" />
+                  </button>
+                  <br><br>
+                  <button class="button is-danger is-large" v-on:click="doRemove(item)">
+                    <font-awesome-icon class="icon is-large" :icon="[ 'fas', 'male' ]" />
+                  </button>
+                  <br><br>
+                </div>
+              </div>
+            </div>
+
+            <br><br><br><br><br>
+            
+            <!-- view streat graph -->
+            <div class="streat">
+              <img :src="streat_icon" />
+            </div>
           </div>
         </div>
       </div>
@@ -108,13 +133,15 @@ import { library } from '@fortawesome/fontawesome-svg-core'
 import { fas } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 import { fab, faGithub } from '@fortawesome/free-brands-svg-icons'
+import { SelfBuildingSquareSpinner } from 'epic-spinners/dist/lib/epic-spinners.min.js'
 
 library.add(fab, fas, faGithub)
 
 export default {
   components: {
     draggable,
-    FontAwesomeIcon
+    FontAwesomeIcon,
+    SelfBuildingSquareSpinner,
   },
   data() {
     return {
@@ -127,11 +154,22 @@ export default {
       nextTodoId: 2,                                // list next id
       num: 5,                                       // count number
       pop_flag: false,                              // modle flag
-      error_message: "",
+      error_message: "",                            // error message string
+      first_flag: false,                             // first session flag
+      loading: true,
       signal_icon: require("~/static/signal.png"),
       streat_icon: require("~/static/streat.png"),
       commit_icon: require("~/static/commit.png"),
     };
+  },
+  /*
+    page loader
+  */
+  mounted() {
+    setTimeout(() => {
+      this.loading = false;
+      this.first_flag = true;
+    }, 1000);
   },
   methods: {
     /*
@@ -172,6 +210,9 @@ export default {
       this.nextTodoId--
       var index = this.list.indexOf(item)
       this.list.shift();
+    },
+    desableFirst: function() {
+      this.first_flag = false
     },
   },
 };
